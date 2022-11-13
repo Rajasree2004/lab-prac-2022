9a069b2bcdeeecf17cf932afa515bd9b4661fcb8
Rajasree2004 <rajasree.selva@gmail.com>
2022-11-13 12:49:41 +0530

    abstract class done

---
+++ JAVA_PRAC/ShapeExec.java
@@ -0,0 +1,29 @@
+abstract class Shape{
+    abstract void numOfSides();
+}
+
+class Triangle extends Shape{
+    void numOfSides(){
+        System.out.println("Triangle - 3 sides");
+    }
+}
+
+class Trapezoid extends Shape{
+    void numOfSides(){
+        System.out.println("Trapezoid - 4 sides");
+    }
+}
+
+class Hexagon extends Shape{
+    void numOfSides(){
+        System.out.println("Hexagon - 6 sides");
+    }
+}
+
+class ShapeExec{
+    public static void main(String[] args){
+        new Triangle().numOfSides();
+        new Trapezoid().numOfSides();
+        new Hexagon().numOfSides();
+    }
+}
