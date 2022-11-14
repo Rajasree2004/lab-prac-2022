85e252e43fe0db26c6e5684a83ab691bacbc7195
Rajasree2004 <rajasree.selva@gmail.com>
2022-11-14 14:11:50 +0530

    Selection sort done

---
+++ C_PRAC/11C.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int arr[10]={34,9,58,63,75,7,5};
+    int n=10;
+    int i, j, pos, swap;
+    for(i=0;i<n-1;i++){
+        pos = i;
+        for(j = i+1;j<n;j++){
+            if(arr[pos]>arr[j]){
+                pos = j;
+            }
+            if(pos != i){
+                swap = arr[i];
+                arr[i] = arr[pos];
+                arr[pos] = swap;
+            }
+        }
+    }    
+    for(i=0;i<n;i++){
+        printf("%d\n", arr[i]); //Dont use tab for printing
+    }
+}
